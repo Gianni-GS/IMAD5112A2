f948182ba1d20c060706bb998af50a731b116f53
Gianni Steyn <giannisteyn0604@gmail.com>
2026-04-22 18:41:46 +0200

    Push

--- app/src/main/java/com/example/imad5111a2assignment/MainActivity.kt
+++ app/src/main/java/com/example/imad5111a2assignment/MainActivity.kt
@@ -13,3 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.imad5111a2assignment.ui.theme.IMAD5111A2AssignmentTheme
 
+class MainActivity : ComponentActivity() {
+    private val questions = arrayOf(
+        "Is Football the most popular sport in the world ?",
+        "Does Manchester United have 13 Premier League titles ?",
+        "Does Cristiano Ronaldo have 6 Ballon d'ors ?",
+        "Did Argentina win the 2022 FIFA World Cup in Qatar ?",
+        "Is Real Madrid from France ?",
+    )
